936ef99c853e434bd3b6abd1355b63dfa7f19167
Odair José Gambert Nossa <odairjgn@outlook.com>
2022-03-24 23:05:00 -0300

    ajustes

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -10,4 +10,18 @@ describe("Meus primeiros testes", () => {
   it("should have welcome screen", async () => {
     await expect(element(by.id("welcome"))).toBeVisible();
   });
+
+  it("Check register new skill", async () => {
+    const inputNewSkill = await element(by.id("input-new"));
+    const buttonAdd = await element(by.id("button-add"));
+    const flatListSkills = await element(by.id("flat-list-skills"));
+
+    await inputNewSkill.tap();
+    await inputNewSkill.typeText("Yza");
+    await buttonAdd.tap();
+
+    //if (flatListSkills) await flatListSkills.tap();
+
+    expect(element(by.id("flat-list-skills")).toBeVisible());
+  });
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -55,26 +55,30 @@ export function Home() {
       <Text style={styles.greetings}>{gretting}</Text>
 
       <TextInput
+        testID="input-new"
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
 
-      <Button title="Add" onPress={handleAddNewSkill} />
+      <Button testID="button-add" title="Add" onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>MySkills</Text>
-
-      <FlatList
-        data={mySkills}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <SkillCard
-            skill={item.name}
-            onPress={() => handleRemoveSkill(item.id)}
-          />
-        )}
-      />
+      {mySkills && (
+        <FlatList
+          testID="flat-list-skills"
+          data={mySkills}
+          keyExtractor={(item) => item.id}
+          keyboardShouldPersistTaps="never"
+          renderItem={({ item }) => (
+            <SkillCard
+              skill={item.name}
+              onPress={() => handleRemoveSkill(item.id)}
+            />
+          )}
+        />
+      )}
     </View>
   );
 }
